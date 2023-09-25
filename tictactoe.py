876e145271185474ef094a015114b27c020246de
dufalcon17 <83681963+dufalcon17@users.noreply.github.com>
2023-09-24 19:29:25 -0600

    Add files via upload
    Beginning of the project. Has the initial variables and print_board function.

---
+++ tictactoe.py
@@ -0,0 +1,23 @@
+# joshua Younger
+
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+current_player = "x"
+winner = None
+game_Running = True
+
+#print board
+def print_board(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+print_board(board)
+
+#player input
+
+#check board
+
+#switch player
+
+#check board
